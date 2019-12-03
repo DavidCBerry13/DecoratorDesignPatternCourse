c444a7f7779457799f10943afac8ac6ac64fc990
David Berry <dberry83702@yahoo.com>
2019-12-03 02:48:44 -0600

    Added Inital version files for .NET Framework

---
+++ dotnetframework/1 - Clip 3 start - Initial project/DecoratorDesignPattern/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DecoratorDesignPattern.OpenWeatherMap;
+using DecoratorDesignPattern.WeatherInterface;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Caching;
+using System.Web.Configuration;
+
+namespace DecoratorDesignPattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWeatherService _weatherService;
+        private readonly Cache _cache;
+
+
+        public HomeController(Cache cache)
+        {
+            _cache = cache;
+
+            String apiKey = WebConfigurationManager.AppSettings["OpenWeatherMapApiKey"];
+            _weatherService = new WeatherService(apiKey);
+        }
+
+        public ActionResult Index(string location = "Chicago")
+        {
+            CurrentWeather conditions = _weatherService.GetCurrentWeather(location);
+            return View(conditions);
+        }
+
+        public ActionResult Forecast(string location = "Chicago")
+        {
+            LocationForecast forecast = _weatherService.GetForecast(location);
+            return View(forecast);
+        }
+
+        public ActionResult ApiKey()
+        {
+            return View();
+        }
+    }
+}
